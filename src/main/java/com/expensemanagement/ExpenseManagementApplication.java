1916b63b4ccdf9a3d4488f9d598afa58c69e1f6a
GabrielBressi <gabrielbressi18@gmail.com>
2024-02-09 11:26:10 -0300

    :tada: Initial Config

---
+++ src/main/java/com/expensemanagement/ExpenseManagementApplication.java
@@ -0,0 +1,13 @@
+package com.expensemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/expensemanagement/ExpenseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expensemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
